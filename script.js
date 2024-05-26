800e1cf7b6e7b8e1b490412e8b17d1ade16d8fb2
Joao Vinicius Rodrigues Siqueira <jvvinicius9@gmail.com>
2024-05-26 18:56:11 -0300

    Atualização tela de cadastro

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-function mudarPag(){
-    window.location.replace
+let divAluno = document.getElementById("cadastroAluno")
+let divMotorista = document.getElementById("cadastroMotorista")
+
+// atualizar tela de cadastro de acordo com o select
+function atualizarSelect() {
+
+    let selectUsuario = document.getElementById("categoriaUsuario")
+    let optionValue = selectUsuario.options[selectUsuario.selectedIndex];
+
+    let value = optionValue.value;
+
+    if (value == 1) {
+        divMotorista.style.display = "none"
+        divAluno.style.display = "contents";
+    } if (value == 2) {
+        divMotorista.style.display = "contents"
+        divAluno.style.display = "none";
+    } else if (value == 0) {
+        divMotorista.style.display = "none"
+        divAluno.style.display = "none"; 
+    }
 }
+
+atualizarSelect()
